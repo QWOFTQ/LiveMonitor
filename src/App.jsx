28f83059686a3371b2c44ef2c37e55350c6bd36f
QWOFTQ <rkdcofls3890@naver.com>
2024-05-27 01:52:03 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+import Guide from "./guide";
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  return (
+    <Router>
+      <div className="grid-container">
+        <Header OpenSidebar={OpenSidebar} />
+        <Sidebar
+          openSidebarToggle={openSidebarToggle}
+          OpenSidebar={OpenSidebar}
+        />
+        <Routes>
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
